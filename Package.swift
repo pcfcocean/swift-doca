6d636706377e4a7d8fcafac2b032951d60a74667
Владислав Лесничий <51000407+pcfcocean@users.noreply.github.com>
2025-11-15 20:49:01 +0300

    some pack changes

--- Package.swift
+++ Package.swift
@@ -1,21 +1,16 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.7
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftDoca",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftDoca",
-            targets: ["SwiftDoca"]),
+            targets: ["SwiftDoca"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SwiftDoca"),
-
+        .target(name: "SwiftDoca")
     ]
 )
